6c50e91573e4574e04d22c283b0b8866ac6e7d3c
Ayanne39 <ayannemachado12@gmail.com>
2025-12-02 18:39:57 -0300

    Update bot1.py

---
+++ bot1.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'Estamos logados como {bot.user}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Olá eu sou um bot {bot.user}!')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+bot.run("Token")
